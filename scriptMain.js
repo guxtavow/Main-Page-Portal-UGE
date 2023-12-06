a17949112855f904bb8d5df7447758aa7f3dfb5a
guxtavow <aquinoavilag@gmail.com>
2023-12-06 16:54:16 -0300

    new indicators menu

--- scriptMain.js
+++ scriptMain.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
-    $('#meuCarousel').on('slid.bs.carousel', function (e) {
+$(document).ready(function () { //Inicio o processo para rolagem do carrossel
+    $('#meuCarousel').on('slid.bs.carousel', function (e) {//trago a id do html e defino uma função de troca de indicadores
         // Remove a classe "active" de todos os indicadores
-        $('.carousel-indicators li').removeClass('active');
+        $('.container').removeClass('active');
 
         // Obtém o índice do slide ativo e adiciona a classe "active" ao indicador correspondente
         var slideIndex = $('#meuCarousel .carousel-item.active').index();
-        $('.carousel-indicators li[data-slide-to="' + slideIndex + '"]').addClass('active');
+        $('.container[data-slide-to="' + slideIndex + '"]').addClass('active');
     });
 
   })
