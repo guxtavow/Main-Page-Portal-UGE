bdbf838e549a858fe0b4aef4560b2e4982cfc352
guxtavow <aquinoavilag@gmail.com>
2023-12-19 10:40:15 -0300

    new items on menu and fix responsability

--- index/scriptMain.js
+++ index/scriptMain.js
@@ -1,11 +1,33 @@
+$(".container[data-slide-to='5'], .container[data-slide-to='6']").hide();
 $(document).ready(function () { //Inicio o processo para rolagem do carrossel
+
     $('#meuCarousel').on('slid.bs.carousel', function (e) {//trago a id do html e defino uma função de troca de indicadores
         // Remove a classe "active" de todos os indicadores
         $('.container').removeClass('active');
 
         // Obtém o índice do slide ativo e adiciona a classe "active" ao indicador correspondente
         var slideIndex = $('#meuCarousel .carousel-item.active').index();
-        $('.container[data-slide-to="' + slideIndex + '"]').addClass('active');
+        var slideAtivo = $('.container[data-slide-to="' + slideIndex + '"]').addClass('active');
+
+
+        const dataSlide1 = $(".container[data-slide-to='4']")
+        const dataSlide2 = $(".container[data-slide-to='5']")
+        const dataSlide3 = $(".container[data-slide-to='6']")
+
+        
+        
+        if(dataSlide1.hasClass("active") || dataSlide2.hasClass("active") || dataSlide3.hasClass("active")) {
+            $(".container[data-slide-to='5'], .container[data-slide-to='6']").show()
+            if(dataSlide3.hasClass("active")){
+                $(".container[data-slide-to='5']").hide()
+                $(".container[data-slide-to='6']").show()
+            }
+        }                  
+        else{
+            $(".container[data-slide-to='5'], .container[data-slide-to='6']").hide()
+        }
+
+
     });
 
   })
