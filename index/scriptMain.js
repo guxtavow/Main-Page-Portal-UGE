56da619e1789a4f43c0ee9a85e7641cccaef4221
guxtavow <aquinoavilag@gmail.com>
2023-12-22 16:26:42 -0300

    add comments about code

--- index/scriptMain.js
+++ index/scriptMain.js
@@ -8,34 +8,35 @@ $(document).ready(function () { //Inicio o processo para rolagem do carrossel
 
         // Obtém o índice do slide ativo e adiciona a classe "active" ao indicador correspondente
         var slideIndex = $('#meuCarousel .carousel-item.active').index();
-        var slideAtivo = $('.container[data-slide-to="' + slideIndex + '"]').addClass('active');
+        $('.container[data-slide-to="' + slideIndex + '"]').addClass('active');
 
-
+        //salvando os index de cada indicador do menu em uma variavel
         const dataSlide = $(".container[data-slide-to='0']")
         const dataSlide1 = $(".container[data-slide-to='4']")
         const dataSlide2 = $(".container[data-slide-to='5']")
         const dataSlide3 = $(".container[data-slide-to='6']")
         const dataSlide4 = $(".container[data-slide-to='8']")
 
-
+        
+        //fazendo um ifelse onde caso o slide 8 estiver com o active, resultar na remoção com animação da tela do primeiro elemento do menu
         if(dataSlide4.hasClass('active')){
             dataSlide.removeClass('slide-in')
             dataSlide.addClass('slide-out');
         }
-        else{
+        else{ //logo assim que n estiver mais com a classe "active", trazer de volta o item
             dataSlide.removeClass('slide-out');
             dataSlide.addClass('slide-in');
         }
 
-
+        //assim que a classe "active" chega no index 5 do menu, ativa mais 2 containers no próprio 
         if(dataSlide1.hasClass("active") || dataSlide2.hasClass("active") || dataSlide3.hasClass("active")) {
             $(".container[data-slide-to='5'], .container[data-slide-to='6']").show()
-            if(dataSlide3.hasClass("active")){
+            if(dataSlide3.hasClass("active")){ //assim que acabar um slide, sumir e deixar apenas o slide seguinte
                 dataSlide2.hide()
                 dataSlide3.show()
             }
         }              
-        else{
+        else{//depois sumir ambos
             $(".container[data-slide-to='5'], .container[data-slide-to='6']").hide()
         }
         
