b825fc7fb0b07640667254bd4ef3081760832259
guxtavow <aquinoavilag@gmail.com>
2024-01-04 15:42:22 -0300

    New layout for menu

--- index/scriptMain.js
+++ index/scriptMain.js
@@ -1,47 +1,21 @@
-$(".container[data-slide-to='5'], .container[data-slide-to='6']").hide();
+document.addEventListener("DOMContentLoaded", function () {
+    abrirMenu() //abrir canvas automatico
+    var botaoAbrir = document.getElementById("abrir"); //pegar elemento do botão de abertura do canvas
+    var botaoFechar = document.getElementById("fechar"); //pegar elemento do botão de fechar do canvas
 
-$(document).ready(function () { //Inicio o processo para rolagem do carrossel
 
-    $('#meuCarousel').on('slid.bs.carousel', function (e) {//trago a id do html e defino uma função de troca de indicadores
-        // Remove a classe "active" de todos os indicadores
-        $('.container').removeClass('active');
+    botaoAbrir.addEventListener("click", function(){ //adiciona um evento que ao clicar o botão: ABRE o canvas
+        menu.style.marginLeft = '-2px';
+    })
+    botaoFechar.addEventListener("click", function(){ //adiciona um evento que ao clicar o botão: FECHA o canvas
+        menu.style.marginLeft = '-310px';
+      })
+});
 
-        // Obtém o índice do slide ativo e adiciona a classe "active" ao indicador correspondente
-        var slideIndex = $('#meuCarousel .carousel-item.active').index();
-        $('.container[data-slide-to="' + slideIndex + '"]').addClass('active');
 
-        //salvando os index de cada indicador do menu em uma variavel
-        const dataSlide = $(".container[data-slide-to='0']")
-        const dataSlide1 = $(".container[data-slide-to='4']")
-        const dataSlide2 = $(".container[data-slide-to='5']")
-        const dataSlide3 = $(".container[data-slide-to='6']")
-        const dataSlide4 = $(".container[data-slide-to='8']")
+var menu = document.getElementById("menu") //pego o elemento "menu" no HTML
 
-        
-        //fazendo um ifelse onde caso o slide 8 estiver com o active, resultar na remoção com animação da tela do primeiro elemento do menu
-        if(dataSlide4.hasClass('active')){
-            dataSlide.removeClass('slide-in')
-            dataSlide.addClass('slide-out');
-        }
-        else{ //logo assim que n estiver mais com a classe "active", trazer de volta o item
-            dataSlide.removeClass('slide-out');
-            dataSlide.addClass('slide-in');
-        }
+function abrirMenu(){ //transformo em uma função para que o OffCanvas apareça automaticamente toda vez que a pagina for aberta
+    menu.style.marginLeft = '-2px'
+}
 
-        //assim que a classe "active" chega no index 5 do menu, ativa mais 2 containers no próprio 
-        
-        
-        if(dataSlide1.hasClass("active") || dataSlide2.hasClass("active") || dataSlide3.hasClass("active")) {
-            $(".container[data-slide-to='5'], .container[data-slide-to='6']").show()
-            if(dataSlide3.hasClass("active")){ //assim que acabar um slide, sumir e deixar apenas o slide seguinte
-                dataSlide2.hide()
-                dataSlide3.show()
-            }
-        }              
-        else{//depois sumir ambos
-            $(".container[data-slide-to='5'], .container[data-slide-to='6']").hide()
-        }
-        
-    });
-    
-  })
